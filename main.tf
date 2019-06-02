b23355e9fe7309170959f483dbcede414bc2250f
Ilya Sotkov <ilya@sotkov.com>
2019-06-02 11:31:41 +0300

    Remove cloud-init references and add basic "init" null_resource

--- main.tf
+++ main.tf
@@ -7,3 +7,26 @@ terraform {
     }
   }
 }
+
+variable "ssh_password" {
+  type = string
+}
+
+resource "null_resource" "init" {
+  triggers = {
+    always = uuid()
+  }
+
+  connection {
+    type     = "ssh"
+    host     = "192.168.10.48"
+    user     = "root"
+    password = var.ssh_password
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get update"
+    ]
+  }
+}
