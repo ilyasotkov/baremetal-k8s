60919ff5e36f9cc7474f0a3b3927eaa03b06b711
Ilya Sotkov <ilya@sotkov.com>
2019-06-02 15:23:19 +0300

    Add config for second connection

--- main.tf
+++ main.tf
@@ -12,14 +12,24 @@ variable "ssh_password" {
   type = string
 }
 
+variable "connections" {
+  type = list
+  default = [
+    "192.168.10.48",
+    "192.168.10.51"
+  ]
+}
+
 resource "null_resource" "init" {
+  count = length(var.connections)
+
   triggers = {
     always = uuid()
   }
 
   connection {
     type     = "ssh"
-    host     = "192.168.10.48"
+    host     = element(var.connections, count.index)
     user     = "root"
     password = var.ssh_password
   }
