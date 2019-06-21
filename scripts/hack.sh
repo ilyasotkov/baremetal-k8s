5db2a91b309ab0e1d3ee61341068df6449afdb4c
Ilya Sotkov <ilya@sotkov.com>
2019-06-21 21:01:16 +0300

    Add configuration for efk stack for log management

--- scripts/hack.sh
+++ scripts/hack.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)/..
 cd ./hack
 
 if [ $1 == all ]; then
-    helmfile -q sync --concurrency=1
+    helmfile --quiet --file helmfile.yaml sync --concurrency=1
 else
-    helmfile -q -l app=$1 sync --concurrency=1
+    helmfile --quiet --file helmfile.yaml --selector app=$1 sync --concurrency=1
 fi
