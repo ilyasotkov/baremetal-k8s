1bffe581ad7ef8554b92a8fde9aff0120b525bb5
Ilya Sotkov <ilya@sotkov.com>
2019-06-22 18:14:06 +0300

    Rename ./scripts/hack.sh to ./scripts/deploy.sh, make it more flexible

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,5 @@ cd ./hack
 if [ $1 == all ]; then
     helmfile --quiet --file helmfile.yaml sync --concurrency=1
 else
-    helmfile --quiet --file helmfile.yaml --selector app=$1 sync --concurrency=1
+    helmfile --quiet --file helmfile.yaml --selector $1 sync --concurrency=1
 fi
