ea48ec596fdc886837568b8ae7a50d438a306b2a
Ilya Sotkov <ilya@sotkov.com>
2019-06-23 08:34:47 +0300

    Update Helmfile version, start using "helmfiles:" for ordering

---
+++ scripts/sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux
+cd $(dirname $0)/..
+
+cd ./hack
+flags=""
+
+if [ $1 == all ]; then
+    helmfile $flags --file helmfile.yaml sync --concurrency=1
+else
+    helmfile $flags --file helmfile.yaml --selector $1 sync --concurrency=1
+fi
